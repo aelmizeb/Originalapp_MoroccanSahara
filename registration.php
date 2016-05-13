f494e4b2122f62f91c414d3462e1b65ef71425c3
aelmizeb <aelmizeb@gmail.com>
2016-05-13 10:46:15 +0100

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2016 Originalapp. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Originalapp_MoroccanSahara',
+    __DIR__
+);
